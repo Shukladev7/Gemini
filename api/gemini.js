d421ad887e04341ebe0cc7c8870d898a853e1b66
Shukladev7 <exit08006@gmail.com>
2025-11-13 22:27:38 +0530

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
       ]
     };
 
-    const response = await fetch( "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + "AIzaSyBooQuwCLfTjrxrZSe9gCPcj558_8_9FP4", {
+    const response = await fetch( "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + "AIzaSyBooQuwCLfTjrxrZSe9gCPcj558_8_9FP4", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
